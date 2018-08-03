701966f50384d91d95e3e8b7db869801faff8c80
Quyen Le Sy <danquyen6596@gmail.com>
2018-08-03 20:50:27 +0700

    get person's name by program aguament

--- src/Hello.java
+++ src/Hello.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Nhap vao ten: ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-
+        String name = args[0];
         System.out.println("Hello " + name + "!");
     }
 }
