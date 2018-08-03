3ea3dddcb6163e893b0dc3b534afc30da919c6fa
Quyen Le Sy <danquyen6596@gmail.com>
2018-08-03 20:42:19 +0700

    Hello A Specific Person instead of Whole World.

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Nhap vao ten: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
     }
 }
